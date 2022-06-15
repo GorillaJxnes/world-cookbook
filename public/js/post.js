9f694d989d4c87e766810d7d5ea07604a8ac00c3
IonicCraze <daviddeitz97@gmail.com>
2022-06-15 10:20:00 -0400

    finished structure of front end js

--- public/js/post.js
+++ public/js/post.js
@@ -1,6 +1,22 @@
-const loginFormHandler = async (event) => {
+const loginFormHandler = async (e) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
-    const blog_title = document.querySelector('#post-title').value.trim();
-    const blog_content = document.querySelector('#post-content').value.trim();
-}
+    e.preventDefault();
+    const name = document.querySelector('').value.trim();
+    const ingredients = document.querySelector('').value.trim();
+    const steps = document.querySelector('').value.trim();
+    const meal = document.querySelector('').value.trim();
+
+    if (name && ingredients && steps && meal) {
+        const response = await fetch('/api/posts/newpost', {
+            method: 'POST',
+            body: JSON.stringify({ name, ingredients, steps, meal }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/')
+        } else {
+            alert('Failed to Post')
+        }
+    }
+};
