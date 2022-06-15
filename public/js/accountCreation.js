60e94ce64d3a2a9083c7961522f1898f5438c701
Nate LaBine <njlabine@gmail.com>
2022-06-15 12:14:34 -0400

    still issues with routes

--- public/js/accountCreation.js
+++ public/js/accountCreation.js
@@ -1,24 +1,23 @@
 const accountCreation = async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Gather the data from the form elements on the page
-    const name = document.querySelector('#user-name').value.trim();
-    const email = document.querySelector('#user-email').value.trim();
-    const password = document.querySelector('#user-password').value.trim();
+  // Gather the data from the form elements on the page
+  const name = document.querySelector("#user-name").value.trim();
+  const email = document.querySelector("#user-email").value.trim();
+  const password = document.querySelector("#user-password").value.trim();
 
-    if (name && email && password) {
-        const response = await fetch('/api/users/newuser', {
-            method: 'Post',
-            body: JSON.stringify({ name,email, password }),
-            headers: { 'Content-Type': 'application/json' },
-        });
-        if (response.ok) {
-            document.location.replace('/');
-        } else {
-            alert('YOO Bros Your Account Creation has failed')
-        }
+  if (name && email && password) {
+    const response = await fetch("/api/users/newuser", {
+      method: "Post",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert("Account creation has failed!");
     }
+  }
 };
 
-
-document.addEventListener('submit', newAccount)
+document.addEventListener("submit", accountCreation);
