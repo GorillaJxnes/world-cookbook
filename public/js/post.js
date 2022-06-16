6081bd438e5712eac8a8ca1a78c925413ddd82cd
IonicCraze <daviddeitz97@gmail.com>
2022-06-16 11:57:41 -0400

    updated post form

--- public/js/post.js
+++ public/js/post.js
@@ -1,10 +1,10 @@
 const loginFormHandler = async (e) => {
     // Stop the browser from submitting the form so we can do so with JavaScript
     e.preventDefault();
-    const name = document.querySelector('').value.trim();
-    const ingredients = document.querySelector('').value.trim();
-    const steps = document.querySelector('').value.trim();
-    const meal = document.querySelector('').value.trim();
+    const name = document.querySelector('#post-title').value.trim();
+    const ingredients = document.querySelector('#post-ingredients').value.trim();
+    const steps = document.querySelector('#post-steps').value.trim();
+    const meal = document.querySelector('#meal').value.trim();
 
     if (name && ingredients && steps && meal) {
         const response = await fetch('/api/posts/newpost', {
