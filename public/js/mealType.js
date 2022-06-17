f3b741f36b3fa6b99530f2d979646d3a63504ff6
BillH <wmhhardgroveiv@gmail.com>
2022-06-17 13:08:04 -0400

    acceping data

--- public/js/mealType.js
+++ public/js/mealType.js
@@ -1,22 +1,21 @@
+const mealChoices = {
+    dinner: 'dinner',
+    lunch: 'lunch',
+    breakfast: 'breakfast'
+  };
 
-    const mealChoices = {
-        dinner: 'dinner',
-        lunch: 'lunch',
-        breakfast: 'breakfast'
-      };
-
-    const mealChoice = (choice) => {
-        const url = /models/recipe.js/$,{choice}
-        fetch(url)
-      .then(response => response.json())
-      .then(data => console.log(data));;
-    };
+const mealChoice = (choice) => {
+    const url = /models/recipe.js/${choice}
+    fetch(url)
+  .then(response => response.json())
+  .then(data => console.log(data));;
+};
 
 
-    router.post('/recipe', async (req,res) => {
-        if (choice === mealChoices.dinner) {
-        } else if (choice === mealChoices.lunch) {
-        } else if (choice === mealChoices.breakfast) {
-        } else {
-      
-    }};
+router.post('/recipe', async (req,res) => {
+    if (choice === mealChoices.dinner) {
+    } else if (choice === mealChoices.lunch) {
+    } else if (choice === mealChoices.breakfast) {
+    } else {
+    }
+});
